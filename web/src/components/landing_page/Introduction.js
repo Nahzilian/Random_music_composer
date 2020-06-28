2ae52f1ed5e0af12d06e8ce9850bcfdc6d9c011b
Nahzilian <duc.nguyen@ryerson.ca>
2020-06-28 00:11:25 -0400

    Added FadeIn animation

--- web/src/components/landing_page/Introduction.js
+++ web/src/components/landing_page/Introduction.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import Logo from '../../assets/headphone.png'
+import FadeIn from 'react-fade-in'
 
 var center = {
     textAlign:"center",
@@ -10,25 +11,27 @@ var center = {
 export class Introduction extends Component {
     render() {
         return (
-            <div className = "container center-block">
-                <div className = "row">
-                    <div className = "col-4">
-                        <img src = {Logo} width="100%" height = "auto"/>
-                    </div>
-                    <div className = "col-8" style = {center}>
-                        <div className = "introduction">
-                            <h2>Random Music Composer</h2>
-                            <br/>
-                            <p>
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                                Duis est nunc, ultricies pretium neque et, cursus sollicitudin orci. 
-                                Quisque sodales diam vitae tristique molestie.
-                            </p>
-                            <button className = "btn-rounded"><i>Generate now!</i></button>
+            <FadeIn>
+                <div className = "container center-block">
+                    <div className = "row">
+                        <div className = "col-4">
+                            <img src = {Logo} width="100%" height = "auto"/>
+                        </div>
+                        <div className = "col-8" style = {center}>
+                            <div className = "introduction">
+                                <h2>Random Music Composer</h2>
+                                <br/>
+                                <p>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                                    Duis est nunc, ultricies pretium neque et, cursus sollicitudin orci. 
+                                    Quisque sodales diam vitae tristique molestie.
+                                </p>
+                                <button className = "btn-rounded"><i>Generate now!</i></button>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
+            </FadeIn>
         )
     }
 }
